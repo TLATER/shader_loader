0bab4063a4a925d14b8c61271980e22241c722f5
Tristan Maat <tm@tlater.net>
2016-10-05 19:42:44 +0100

    Fix missing include guard

--- include/shader_error.hpp
+++ include/shader_error.hpp
@@ -1,3 +1,6 @@
+#ifndef SHADER_ERROR_HPP
+#define SHADER_ERROR_HPP
+
 #include <stdexcept>
 
 class ShaderError: public std::runtime_error
@@ -5,3 +8,5 @@ class ShaderError: public std::runtime_error
 public:
     ShaderError(const char* message): std::runtime_error(message) {};
 };
+
+#endif
