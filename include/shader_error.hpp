59e883e93e789afbe61b20063c5554300a9f1fd7
Tristan Maat <tm@tlater.net>
2016-12-11 21:26:39 +0000

    Add ShaderError(std::string)

--- include/shader_error.hpp
+++ include/shader_error.hpp
@@ -1,11 +1,13 @@
 #ifndef SHADER_ERROR_HPP
 #define SHADER_ERROR_HPP
 
+#include <string>
 #include <stdexcept>
 
 class ShaderError: public std::runtime_error
 {
 public:
+    ShaderError(std::string message): ShaderError(message.c_str()) {};
     ShaderError(const char* message): std::runtime_error(message) {};
 };
 
